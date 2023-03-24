3db8894f33c8ccafaced44bb584c634c5bdfa5b1
devcharle <charlexavier100@gmail.com>
2023-03-23 22:52:48 -0300

    localstorage concluido

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 // -identificando-os-elementos-no-html-através-do-id dos botoes
+let btnNavCategoria = document.getElementById("categorias");
+let btnNavDespesas = document.getElementById("despesas");
 let btnFiltrar = document.getElementById("botao-filtrar");
 let btnAdicionardespesa = document.getElementById("botao-adicionar");
 let btnSalvardespesa = document.getElementById("botao-salvar-despesa");
@@ -7,12 +9,11 @@ let btnCancelar = document.getElementById("botao-cancelar");
 let btnCancelaDespesa = document.getElementById("botao-cancelar-despesa");
 let btnCancelarCategoria = document.getElementById("btn-categoria-cancelar");
 let btnAddcategoria = document.getElementById("addcategoria");
-let btnNavCategoria = document.getElementById("categorias");
-let btnNavDespesas = document.getElementById("despesas");
 let btnFiltrarCadastro = document.getElementById("btn-filtrar-cadastro");
 let btnFiltrarDespesa = document.getElementById("botao-filtrar");
-let btnPago = document.getElementById("pago");
+// let btnPago = document.querySelector("#pago");
 let btnPendente = document.getElementById("pendente");
+// troca de paginas
 let paginaHome = document.getElementById("pagina-home");
 let paginaAddDespesas = document.getElementById("pagina-despesas");
 let paginaCadastroCategoria = document.getElementById("pagina-cadastro");
@@ -30,10 +31,25 @@ let inputData = document.getElementById("data");
 let inputDespesas = document.getElementById("despesa");
 let inputValor = document.getElementById("valor");
 let inputFiltroDespesas = document.getElementById("filtro-despesas");
-let opcoesCategorias = document.getElementById("opcoes-categorias");
-const listadeCategorias = [];
 
-function gerarId() {
+// data list
+let opcoesCategorias = document.getElementById("opcoes-categorias");
+
+// localStorage
+let listadeCategoriaslocalstorage = JSON.parse(
+  localStorage.getItem("arraylistacategorias")
+);
+
+const listadeCategorias = listadeCategoriaslocalstorage ?? [];
+
+let listaDespesaslocalStorage = localStorage.getItem("despesas")
+  ? JSON.parse(localStorage.getItem("despesas"))
+  : [];
+
+const listaDespesas = listaDespesaslocalStorage ?? [];
+
+// gerando id para as listas de categoria e dispesas 
+function gerarIdCategoria() {
   if (listadeCategorias.length > 0) {
     return listadeCategorias[listadeCategorias.length - 1].id + 1;
   }
@@ -52,6 +68,7 @@ function validarFormulario() {
     listadeCategorias.find((obj) => obj.categoria == inputCategoria.value)
   ) {
     alert("ERRO digite novamente ");
+    
   } else {
     inputCategoria.value = inputCategoria.value.toLowerCase(); // converter para minúsculas
     alert("preenchido com sucesso");
@@ -59,16 +76,17 @@ function validarFormulario() {
     limparCampo();
   }
 }
-
+  // objeto categoria
 function salvarCategoria() {
   let categoria = {
-    id: gerarId(),
+    id: gerarIdCategoria(),
     categoria: inputCategoria.value,
   };
 
   listadeCategorias.push(categoria);
   console.log(listadeCategorias);
 }
+
 // Remove a categoria da lista
 function excluirCategoria(id) {
   const categoriaIndex = listadeCategorias.findIndex(
@@ -77,28 +95,32 @@ function excluirCategoria(id) {
 
   if (categoriaIndex !== -1) {
     listadeCategorias.splice(categoriaIndex, 1);
-    listarCategoria();
+    listarCategoria(listadeCategorias);
   }
 }
-// funcao pra filtrar categorias e exibir
-function exibeLista(categoria) {
-  // lista filtrada
-  let trTds = "";
-  categoria.forEach(function (categoria) {
-    trTds += `<tr>
-    <td>${categoria.id}</td>
-    <td>${categoria.categoria}</td>
-   <td class="botoes-categoria">
-    <button class="btn-salvar btn-primary" data-id="${categoria.id}" onclick="editarCategoria(${categoria.id})">Editar</button>
-    
-    
-      <button class="btn-cancelar botao-cancelar-pagina" data-id="${categoria.id}" onclick="excluirCategoria(${categoria.id})">Excluir</button>
-    </td>
-    
-    </tr>`;
-  });
-  tabelalistadeCategorias.innerHTML = trTds;
+// excluindo despesa na listaDespesas
+function excluirDespesa(id) {
+  const despesaIndex = listaDespesas.findIndex((despesa) => despesa.id === id);
+
+  if (despesaIndex !== -1) {
+    listaDespesas.splice(despesaIndex, 1);
+    listarDespesa(listaDespesas);
+  }
 }
+
+function atualizandoLocalstorage() {
+  localStorage.setItem(
+    "arraylistacategorias",
+    JSON.stringify(listadeCategorias)
+  );
+}
+function atualizandoLocalstoragedespesas() {
+  localStorage.setItem("despesas", JSON.stringify(listaDespesas));
+}
+// carrega as despesas da tabela do LocalStorage quando a página é carregada
+window.onload = function () {
+  listarDespesa();
+};
 
 let proximoId = 1;
 
@@ -109,10 +131,10 @@ function adicionarCategoria() {
   };
   listadeCategorias.push(novaCategoria);
   proximoId++;
-  listarCategoria();
+  listarCategoria(listadeCategorias);
   inputCategoria.value = "";
 }
-
+// editando a categoria na pagina cadastro de categorias
 function editarCategoria(id) {
   const categoriaParaEditar = listadeCategorias.find(
     (categoria) => categoria.id === id
@@ -122,11 +144,12 @@ function editarCategoria(id) {
     categoriaParaEditar.categoria
   );
   categoriaParaEditar.categoria = novoNomeCategoria;
-  listarCategoria();
+  listarCategoria(listadeCategorias);
 }
 
-function listarCategoria() {
-  tabelalistadeCategorias.innerHTML = listadeCategorias
+// tabela listar categorias 
+function listarCategoria(novalista) {
+  tabelalistadeCategorias.innerHTML = novalista
     .map((categoria) => {
       if (categoria.categoria !== "") {
         return `
@@ -139,7 +162,7 @@ function listarCategoria() {
             <button class="btn-salvar btn-primary" data-id="${categoria.id}" onclick="editarCategoria(${categoria.id})">Editar</button>
             
             
-              <button class="btn-cancelar botao-cancelar-pagina" data-id="${categoria.id}" onclick="excluirCategoria(${categoria.id})">Excluir</button>
+              <button class="btn-cancelar botao-cancelar-pagina altera-btn-cancel" data-id="${categoria.id}" onclick="excluirCategoria(${categoria.id})">Excluir</button>
             </td>
             
           </tr>
@@ -149,6 +172,7 @@ function listarCategoria() {
       }
     })
     .join("");
+  atualizandoLocalstorage();
 }
 function categoriasDatalist() {
   let opcao = "";
@@ -159,55 +183,21 @@ function categoriasDatalist() {
   opcoesCategorias.innerHTML = opcao;
 }
 
-function atualizarCategoria(id) {
-  const categoriaIndex = listadeCategorias.findIndex(
-    (categoria) => categoria.id === id
-  );
-  if (categoriaIndex !== -1) {
-    // Atualize a categoria na lista de categorias com o novo nome da categoria
-    listadeCategorias[categoriaIndex].categoria = inputCategoria.value;
-
-    // Atualize a tabela
-    // listarCategoria();
-
-    // Limpe o formulário e altere o botão "Atualizar" para "Salvar"
-    inputCategoria.value = "";
-    btnSalvar.innerText = "Salvar";
-    btnSalvar.onclick = salvarCategoria;
-  }
-}
-
-// modal adicionando despesa
 function validarFormularioDespesa() {
   if (
-    (inputBuscaCategoria.value.length !== 0 &&
-      isNaN(inputBuscaCategoria.value)) ||
-    (inputDespesas.value.length !== 0 && isNaN(inputDespesas.value.length))
+    inputBuscaCategoria.value.length !== 0 &&
+    inputDespesas.value.length !== 0 &&
+    listadeCategorias.some((obj) => obj.categoria == inputBuscaCategoria.value)
   ) {
     inputBuscaCategoria.value = inputBuscaCategoria.value.toLowerCase(); // converter para minúsculas
-    alert("preenchido com sucesso");
+    alert("preenchido com sucesso")
+    return true;
   } else {
     alert("ERRO digite novamente ");
+    limparFormulario();
+    return false;
   }
 }
-// Seleciona o campo de entrada de data
-// let dataInput = document.getElementById("data");
-
-// // Aplica a máscara no evento "input"
-// dataInput.addEventListener("input", function () {
-//   // Obtém o valor atual do campo de entrada
-//   let data = this.value;
-
-//   // Remove todos os caracteres que não são números
-//   data = data.replace(/\D/g, "");
-
-//   // Adiciona os caracteres da máscara
-//   data = data.replace(/^(\d{2})(\d)/g, "$1/$2");
-//   data = data.replace(/(\d{2})(\d)/, "$1/$2");
-
-//   // Define o valor formatado no campo de entrada
-//   this.value = data;
-// });
 
 // formatando moeda
 let mascara = (symbol, element) => {
@@ -223,26 +213,22 @@ let mascara = (symbol, element) => {
   // Adiciona o símbolo e atualiza o valor do elemento
   inputValor.value = symbol + val;
 };
-// adicionando despesa
 
-const listaDespesas = [];
 
 let despesa = {
-  
   BuscaCategoria: "",
   despesa: "",
-  valor: inputValor.value,
-  data: inputData.value,
+  valor: inputData.value,
+  data: inputValor.value,
 };
 function salvarDespesa() {
   let creandoDespesa = { ...despesa };
-  creandoDespesa.id=gerarIddespesas()
+  creandoDespesa.id = gerarIddespesas();
   creandoDespesa.BuscaCategoria = inputBuscaCategoria.value;
   creandoDespesa.despesa = inputDespesas.value;
   creandoDespesa.valor = inputValor.value;
   creandoDespesa.data = inputData.value;
   listaDespesas.push(creandoDespesa);
-  console.log(listaDespesas);
 }
 
 
@@ -258,18 +244,18 @@ function trocaStatuspago(id) {
     );
     if (pagoButton && pendenteButton) {
       pagoButton.classList.toggle("hidden");
+      somaStatus();
       pendenteButton.classList.toggle("hidden");
+      
     }
   }
 }
 
 // tabela listar despesa
-function listarDespesa() {
-  tabelaControleDedespesas.innerHTML = listaDespesas
-    .map((despesa) => {
-      if (despesa.despesa !== "") {
-        console.log(despesa)
-        return `
+function listarDespesa(listaFiltrodespesas) {
+  tabelaControleDedespesas.innerHTML = listaDespesas.map((despesa) => {
+    if (despesa.despesa !== "") {
+      return `
           <tr>
             <td>${despesa.data}</td>
             <td>${despesa.despesa}</td>
@@ -278,73 +264,65 @@ function listarDespesa() {
               <button btn-Status id="pago" class="btn-salvar btn-primary${
                 despesa.pago ? "" : " hidden"
               }" data-id="${despesa.id}" onclick="trocaStatuspago(${
-          despesa.id
-        })">Pago</button>
+        despesa.id
+      })">Pago</button>
               <button btn-Status id="pendente" class="btn-cancelar botao-cancelar-pagina${
                 !despesa.pago ? "" : " hidden"
               }" data-id="${despesa.id}" onclick="trocaStatuspago(${
-          despesa.id
-        })">Pendente</button>
+        despesa.id
+      })">Pendente</button>
             </td>
+            <button class="btn-cancelar botao-cancelar-pagina" data-id="${
+              despesa.id
+            }" onclick="excluirDespesa(${despesa.id})">Excluir</button>
           </tr>
         `;
-      } else {
-        return "";
-      }
-    })
-    .join("");
+    } else {
+      return "";
+    }
+  });
+
+  atualizandoLocalstoragedespesas();
 }
-// exibe lista filtro
+
+// exibe lista filtro depesa  obs: recebendo  listaFiltrodespesas 
 function exibeListaFiltro(despesa) {
-  // lista filtrada
   let trTdsfiltro = "";
   despesa.forEach(function (despesa) {
     trTdsfiltro += `<tr>
     <td>${despesa.data}</td>
     <td>${despesa.despesa}</td>
     <td>${despesa.valor}</td>
-    
+    <button class="btn-cancelar botao-cancelar-pagina" data-id="${despesa.id}" onclick="excluirDespesa(${despesa.id})">Excluir</button>
     </tr>`;
   });
   tabelaControleDedespesas.innerHTML = trTdsfiltro;
 }
 
-//  Filtrando valor,data,despesas
+//  Filtrando valor,data,despesa
 inputFiltroDespesas.addEventListener("keyup", () => {
   let valordespesa = inputFiltroDespesas.value.toLowerCase().trim();
   let listaFiltrodespesas = listaDespesas.filter((despesa) => {
-    let comparaData = despesa.data.toLowerCase()
-    .includes(valordespesa);
-    let comparaDespesas = despesa.despesa.toLowerCase()
-      .includes(valordespesa);
-    let comparavalor = despesa.valor.toLowerCase()
-    .includes(valordespesa);
+    let comparaData = despesa.data.toLowerCase().includes(valordespesa);
+    let comparaDespesas = despesa.despesa.toLowerCase().includes(valordespesa);
+    let comparavalor = despesa.valor.toLowerCase().includes(valordespesa);
     return comparaData || comparaDespesas || comparavalor;
   });
+
   exibeListaFiltro(listaFiltrodespesas);
 });
-// inputFiltroDespesas.addEventListener("keyup", () => {
-//   let textoFiltrodespesa = inputFiltroDespesas.value.toLowerCase().trim();
-//   let listaFiltrodespesas = listaDespesas.filter((obj) => {
-//     despesa.despesa.toLowerCase().trim().includes(textoFiltrodespesa)||
-//     despesa.valor.trim().includes(textoFiltrodespesa)||
-//     despesa.data.trim().includes(textoFiltrodespesa)
-
-//   });
-//   exibeListaFiltro(listaFiltrodespesas);
-// });
 
 function limparFormulario() {
-  document.getElementById("meuFormulario").reset(); // reinicia o formulário inteiro
+  document.getElementById("meuFormulario").reset();
 }
 
 btnSalvardespesa.addEventListener("click", () => {
-  validarFormularioDespesa();
-  salvarDespesa();
-  limparFormulario();
-  mostraPaginaHome();
-  listarDespesa();
-  // exibeListaDespesa();
+  if (validarFormularioDespesa()) {
+    salvarDespesa();
+    limparFormulario();
+    mostraPaginaHome();
+    listarDespesa(listaDespesas);
+  }
 });
 
 btnFiltrarCadastro.addEventListener("click", () => {
@@ -352,6 +330,7 @@ btnFiltrarCadastro.addEventListener("click", () => {
   mostraPaginaCategorias();
 });
 
+// input filtro de categorias comparando categoria e id
 inputFiltro.addEventListener("keyup", () => {
   let valor = inputFiltro.value.toLowerCase().trim();
   let listaFiltrada = listadeCategorias.filter((categoria) => {
@@ -359,20 +338,18 @@ inputFiltro.addEventListener("keyup", () => {
     let comparacaoId = categoria.id.toString() === valor;
     return comparaCategoria || comparacaoId;
   });
-  exibeLista(listaFiltrada);
+  listarCategoria(listaFiltrada);
 });
 
-function atualizaTela() {
-  location.reload(); // voltando ao formulario
-}
 function limparCampo() {
   inputCategoria.value = "";
 }
 function mensagem() {
   alert("por favor edite sua categoria na tela anterior");
 }
-// troca de paginas
+// funcao troca de paginas
 function mostraPaginaCategorias() {
+  listarCategoria(listadeCategorias);
   limparCampo();
   paginaHome.classList.add("hidden");
   paginaCadastroCategoria.classList.remove("hidden");
@@ -383,7 +360,10 @@ function mostraPaginaCategorias() {
 btnNavCategoria.addEventListener("click", mostraPaginaCategorias);
 btnCancelarCategoria.addEventListener("click", mostraPaginaHome);
 
+// funcao troca de paginas
 function mostraPaginaHome() {
+  listarDespesa(listaDespesas);
+  limparCampo();
   paginaHome.classList.remove("hidden");
   paginaAdicionarCategoria.classList.add("hidden");
   paginaAddDespesas.classList.add("hidden");
@@ -391,6 +371,7 @@ function mostraPaginaHome() {
 }
 btnNavDespesas.addEventListener("click", mostraPaginaHome);
 
+// funcao troca de paginas
 function mostraModaldespesa() {
   paginaAddDespesas.classList.remove("hidden");
   paginaHome.classList.add("hidden");
@@ -400,6 +381,7 @@ btnAdicionardespesa.addEventListener("click", () => {
   categoriasDatalist();
 });
 
+// funcao troca de paginas
 function mostraCadastroCategoria() {
   paginaAdicionarCategoria.classList.remove("hidden");
   paginaHome.classList.add("hidden");
@@ -413,9 +395,27 @@ btnCancelarCategoria.addEventListener("click", mostraPaginaCategorias);
 btnSalvar.addEventListener("click", () => {
   validarFormulario();
   mostraPaginaCategorias();
-  listarCategoria();
+  listarCategoria(listadeCategorias);
 });
-
+btnFiltrarDespesa.addEventListener("click", () => {
+  validarFormulario();
+});
 btnCancelaDespesa.addEventListener("click", () => {
   mostraPaginaHome();
 });
+
+
+// quando clicar no botao pago chama a 
+// funcao que soma valor e mostra na strong
+
+function somaStatus() {
+  
+const totalDespesas = listaDespesas.reduce((total, despesa) => {
+  return total + despesa.valor ;
+  
+}, 0);
+
+console.log(totalDespesas); 
+
+  
+}
